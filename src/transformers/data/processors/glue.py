bc7736788a16a5bbf443210272d107bd6f2e6697
S Rai <sumedharai12@gmail.com>
2020-03-15 02:38:02 -0400

    NLU3

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -185,7 +185,7 @@ class BoolqProcessor(DataProcessor):
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_json(os.path.join(data_dir, "dev.jsonl")), "dev")
+        return self._create_examples(self._read_json(os.path.join(data_dir, "val.jsonl")), "dev")
 
     def get_labels(self):
         """See base class."""
